4ea06d5d9479f5a34ecefe09b6ac496acf8d1ea7
astaJun <astaJun@users.noreply.github.com>
2023-03-06 05:35:31 +0000

    deploy: eff2d72bce951c01fd2cc0e9b002aacf71dac5eb

---
+++ assets/chunks/TestTailwind.4c5ae9c9.js
@@ -0,0 +1,1 @@
+import{_ as e,o as c,c as n}from"../app.2e15d874.js";const o={},_={class:"p-5 bg-green"};function s(t,r){return c(),n("div",_,"hello")}const l=e(o,[["render",s]]);export{l as _};
--- assets/chunks/TestTailwind.b397d255.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,o as c,c as n}from"../app.025d6630.js";const o={},_={class:"p-5 bg-green"};function s(t,r){return c(),n("div",_,"hello")}const l=e(o,[["render",s]]);export{l as _};
